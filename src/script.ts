6baeb9f4a0433b3e16821bdd0b04ccccebe23076
Igor <(igorrds1998@gmail.com)>
2022-03-15 15:09:59 -0300

    criando types com interface

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,18 @@
-type User = {
+/* type User = {
     nome: string,
     idade: number
-};
+}; */
+
+// usando o type uma vez q tu o criou ele não é alterável não tem como adicionar propriedades novas e com interface sim
+
+interface User {
+    nome: string
+}
+
+// pode fazer isso e o type não
+interface User {
+    idade: number
+}
 
 function resumo(usuario: User) {
     return `Olá ${usuario.nome}, você tem ${usuario.idade} anos`;
