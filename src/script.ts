7ff0b42197e8eecfd5a261512217620e8a9f696f
Igor <(igorrds1998@gmail.com)>
2022-03-15 13:08:18 -0300

    propriedades opcionais

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,9 @@
-function resumo(usuario) {
+function resumo(usuario: {nome: string, idade?: number}) { // colocar um item opcional "?" logo apos da propriedade
+    if(usuario.idade !== undefined) {
     return `Olá ${usuario.nome}, tudo bem? você tem ${usuario.idade} anos`;
+    } else {
+        return `Olá ${usuario.nome}, tudo bem?`;
+    }
 }
 
 let u = {
